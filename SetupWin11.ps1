2469750efd7b2a490bfab6e9dbb1f830c2488bfa
Joe Scalise <joe-scalise@users.noreply.github.com>
2022-07-26 15:58:46 -0400

    Update SetupWin11.ps1

--- SetupWin11.ps1
+++ SetupWin11.ps1
@@ -1,31 +1,43 @@
+<#
+.Synopsis
+Assist in setting up Windows 11.
+#>
+
+[CmdletBinding()]
+param()
+
 $bloatwareList = @(
-    "Microsoft.3DBuilder", "Microsoft.Microsoft3DViewer", "Microsoft.AppConnector",
-    "Microsoft.BingFinance", "Microsoft.BingNews", "Microsoft.BingSports",
-    "Microsoft.BingTranslator", "Microsoft.BingWeather", "Microsoft.BingFoodAndDrink",
-    "Microsoft.BingHealthAndFitness", "Microsoft.BingTravel", "Microsoft.GetHelp",
-    "Microsoft.Getstarted", "Microsoft.Messaging", "Microsoft.Microsoft3DViewer",
-    "Microsoft.MicrosoftSolitaireCollection", "Microsoft.NetworkSpeedTest", "Microsoft.Office.Lens",
-    "Microsoft.Office.Sway", "Microsoft.Office.OneNote", "Microsoft.OneConnect",
-    "Microsoft.People", "Microsoft.Print3D", "Microsoft.SkypeApp",
-    "Microsoft.Wallet", "Microsoft.WindowsAlarms", "Microsoft.windowscommunicationsapps",
-    "Microsoft.WindowsFeedbackHub", "Microsoft.WindowsMaps", "Microsoft.WindowsPhone",
-    "Microsoft.ConnectivityStore", "Microsoft.CommsPhone", "Microsoft.ScreenSketch",
-    "Microsoft.ZuneMusic", "Microsoft.ZuneVideo", "Microsoft.YourPhone",
-    "Microsoft.Getstarted", "Microsoft.MicrosoftOfficeHub", "*EclipseManager*",
-    "*ActiproSoftwareLLC*", "*AdobeSystemsIncorporated.AdobePhotoshopExpress*",
-    "*Duolingo-LearnLanguagesforFree*", "*PandoraMediaInc*", "*CandyCrush*",
-    "*BubbleWitch3Saga*", "*Wunderlist*", "*Flipboard*", "*Twitter*",
-    "*Facebook*", "*Royal Revolt*", "*Sway*", "*Speed Test*", "*Dolby*",
-    "*Viber*", "*ACGMediaPlayer*", "*Netflix*", "*OneCalendar*",
-    "*LinkedInforWindows*", "*HiddenCityMysteryofShadows*", "*Hulu*",
-    "*HiddenCity*", "*AdobePhotoshopExpress*", "*HotspotShieldFreeVPN*",
-    "*Microsoft.Advertising.Xaml*", "MicrosoftTeams*", "Microsoft.OneDriveSync",
-    "Microsoft.Todos", "Microsoft.549981C3F5F10_8wekyb3d8bbwe", "Microsoft.Windows.PeopleExperienceHost"
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather",
+    "Microsoft.GetHelp",
+    "Microsoft.Getstarted",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.People",
+    "Microsoft.WindowsAlarms",
+    "microsoft.windowscommunicationsapps",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.ScreenSketch",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo",
+    "Microsoft.YourPhone",
+    "MicrosoftTeams",
+    "Microsoft.OneDriveSync",
+    "Microsoft.Todos",
+    "Microsoft.Windows.PeopleExperienceHost"    
 )
+
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+	Write-Warning "You do not have Administrator rights"
+    exit
+}
+
+$ErrorActionPreference = "Stop"
 
 # Get the latest version of PowerShell installed
 if ($PSVersionTable.PSVersion.Major -lt 7) {
-    iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
+    Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
 }
 
 # Uninstall Bloatware-Apps
